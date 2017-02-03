da8c3319de902c844ca056db31c55d8a64c93d29
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2017-02-03 01:42:18 +0000

    Merging works but some tiles have glitches

--- mbtiles/views.py
+++ mbtiles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from pyMbTiles import MBTiles
-import zlib
+from pyMbTiles import MBTiles, VectorTile
+import zlib, cStringIO
 
 def index(request):
 	coast = MBTiles.MBTiles("coast.mbtiles")
@@ -9,21 +9,42 @@ def index(request):
 	return HttpResponse("foobar")
 
 def tiles(request, tileZoom, tileColumn, tileRow):
+	tileZoom = int(tileZoom)
+	tileColumn = int(tileColumn)
+	tileRow = int(tileRow)
+
 	coastTiles = MBTiles.MBTiles("coast.mbtiles")
 	coastTile = coastTiles.GetTile(tileZoom, tileColumn, tileRow)
+	coastUncompressed = zlib.decompress(coastTile, 16+zlib.MAX_WBITS)
 
 	mapTiles = MBTiles.MBTiles("map.mbtiles")
-	mapTile = mapTiles.GetTile(tileZoom, tileColumn, tileRow)
+	try:
+		mapTile = mapTiles.GetTile(tileZoom, tileColumn, tileRow)
+		mapUncompressed = zlib.decompress(mapTile, 16+zlib.MAX_WBITS)
+	except RuntimeError:
+		mapUncompressed = None
 
 	#Merge these
-	#uncompressed = zlib.decompress(tile, 16+zlib.MAX_WBITS)
-
-
-
+	data = cStringIO.StringIO()
+	enc = VectorTile.EncodeVectorTile(tileZoom, tileColumn, tileRow, data)
+	encFilterFinish = VectorTile.FilterFinish(enc)
 	
+	dec = VectorTile.DecodeVectorTile(tileZoom, tileColumn, tileRow, encFilterFinish)
+	dec.DecodeTileData(coastUncompressed)
+	if mapUncompressed is not None:
+		dec.DecodeTileData(mapUncompressed)
+	enc.Finish()
+
+	#Recompress
+	comp = zlib.compressobj(zlib.Z_DEFAULT_COMPRESSION, zlib.DEFLATED, 16+zlib.MAX_WBITS)
+	compData = [comp.compress(data.getvalue())]
+	compData.append(comp.flush())
+	recompressedData = b"".join(compData)
+
 	#Send response
-	response = HttpResponse(mapTile, content_type='application/vnd.mapbox-vector-tile')
+	response = HttpResponse(recompressedData, content_type='application/vnd.mapbox-vector-tile')
 	response['Content-Encoding'] = 'gzip'
-	response['Content-Length'] = str(len(mapTile))
+	response['Content-Length'] = str(len(recompressedData))
+
 	return response
 
